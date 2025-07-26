fe9d46cb65b5b026562e21673938469ae7d53a97
Flywith24 <fly.with24@gmail.com>
2025-07-26 15:14:27 +0800

    更新描述

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -17,28 +17,36 @@ const YearsStat = ({
   yearsArrayUpdate.unshift(year);
 
   // for short solution need to refactor
+  // @ts-ignore
   return (
-    <div className="w-full pb-16 pr-16 lg:w-full lg:pr-16">
-      <section className="pb-0">
-        <p className="leading-relaxed">
+    <div className="fl w-100-l pb5 pr5-l">
+      <section className="pb4" style={{ paddingBottom: '0rem' }}>
+        <p style={{ lineHeight: 1.8 }}>
           {INFO_MESSAGE(years.length, year)}
           <br />
+          <br />
+          "明明这么痛苦，这么难过，为什么就是不能放弃跑步？因为全身细胞都在蠢蠢欲动，想要感受强风迎面吹拂的滋味。"
+          <br />
+          <p style={quoteStyle}>&ndash;&ndash;《强风吹拂》</p>
         </p>
       </section>
-      <hr />
+      <hr color="red" />
       {yearsArrayUpdate.map((year) => (
         <YearStat key={year} year={year} onClick={onClick} />
       ))}
-      {
-        // eslint-disable-next-line no-prototype-builtins
+      {// eslint-disable-next-line no-prototype-builtins
         yearsArrayUpdate.hasOwnProperty('Total') ? (
           <YearStat key="Total" year="Total" onClick={onClick} />
         ) : (
           <div />
-        )
-      }
+        )}
     </div>
   );
+};
+
+const quoteStyle = {
+  fontWeight:"bold",
+  textAlign: "right"
 };
 
 export default YearsStat;
--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,8 +16,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Flywith24 Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://www.strava.com/athletes/117781024',
+  logo: 'https://i.postimg.cc/Fs2kN2HY/2025-07-26-3-13-06.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
